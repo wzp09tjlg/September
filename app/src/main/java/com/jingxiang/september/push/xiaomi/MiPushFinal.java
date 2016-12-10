f4c9e5feb80e6549ece4314c0b9efc0431cccfda
wuzp <wzp09tjlg@163.com>
2016-12-10 18:42:30 +0800

    1.添加小米推送的文件

---
+++ app/src/main/java/com/jingxiang/september/push/xiaomi/MiPushFinal.java
@@ -0,0 +1,13 @@
+package com.jingxiang.september.push.xiaomi;
+
+/**
+ * Created by wu on 2016/11/8.
+ * 小米推送的常量
+ */
+public class MiPushFinal {
+
+    public static final String MI_PUSH_APPID     = "2882303761517523204";
+    public static final String MI_PUSH_APPKEY    = "5621752375204";
+    public static final String MI_PUSH_APPSECRET = "ZRp+NhHmWGpyi7gx4yQdgw==";
+
+}
