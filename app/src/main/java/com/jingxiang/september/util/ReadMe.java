e3b60c8060f48b856145dcf262aedcf93ffc6979
wuzp <wzp09tjlg@163.com>
2016-12-10 18:41:28 +0800

    1.添加read.me文件，删除DownService 和 PlayService文件

---
+++ app/src/main/java/com/jingxiang/september/util/ReadMe.java
@@ -0,0 +1,14 @@
+package com.jingxiang.september.util;
+
+/**
+ * Created by wu on 2016/11/8.
+ * 这个类的作用是简单的介绍 应用的做了那些东西，什么时间做的这些东西。
+ */
+public class ReadMe {
+
+    /**
+     * 1.20161108 想添加小米的推送处理
+     * 账号是自己的小米账号，小米推送的版本是2.2版本
+     *
+     * */
+}
